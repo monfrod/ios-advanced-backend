b12867d80209f1405541a3722ebea52826fbbaa1
monfrod <iunus.lalazov.05@gmail.com>
2025-05-13 00:23:31 +0500

    просто добавил принт

--- main.py
+++ main.py
@@ -160,6 +160,7 @@ def get_user_mixes_final():
 
                                 for track_obj in track_objects_from_playlist:
                                     if track_obj:
+                                        print(dir(track_obj))
                                         detailed_tracks_list.append(track_obj.to_dict())
                         except Exception as e_fetch:
                             print(
