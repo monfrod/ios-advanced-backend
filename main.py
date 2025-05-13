a1b8c7b98495c6e30e17c8dd032c88333bc3ea78
monfrod <iunus.lalazov.05@gmail.com>
2025-05-13 10:29:53 +0500

    теперь можно сразу передать ключ в url

--- main.py
+++ main.py
@@ -21,7 +21,13 @@ async def verify_api_key(request: Request, call_next):
     if request.url.path.startswith("/docs") or request.url.path.startswith("/openapi.json"):
         return await call_next(request)
 
+    # Попробовать сначала заголовок
     key = request.headers.get("X-API-KEY")
+
+    # Если нет — попробовать query-параметр
+    if key is None:
+        key = request.query_params.get("apiKey")
+
     if key != API_KEY:
         raise HTTPException(status_code=403, detail="Forbidden: Invalid API Key")
 
